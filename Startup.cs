ef73322226e158421952f783b866a7392618f3e0
HungNV <hungnv6691@seta-international.vn>
2018-06-06 10:21:35 +0700

    up test

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
+
 namespace aspNetCoreCookie
 {
     public class Startup
